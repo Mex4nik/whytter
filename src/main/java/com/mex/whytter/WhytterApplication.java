1d491dd8472a7e5e95c316e8e3822cd1273b5b2d
MeX4NiK <MeX4NiK@ukr.net>
2020-07-28 11:35:51 +0300

    Add project in Git

---
+++ src/main/java/com/mex/whytter/WhytterApplication.java
@@ -0,0 +1,13 @@
+package com.mex.whytter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhytterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhytterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mex/whytter/WhytterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mex.whytter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhytterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
